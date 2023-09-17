c9c850346f9c218bae3f1fd6701691b64e291651
augermjr <137419250+augermjr@users.noreply.github.com>
2023-09-17 15:11:38 -0400

    Initial Commit

---
+++ Lab_04_02_YearlyMaintenanceCost/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCosts = 193.72;
+        double summerCosts = 315.45;
+        double fallCosts = 219.32;
+        double winterCosts = 267.9;
+        double totalCosts = springCosts + summerCosts + fallCosts + winterCosts;
+        System.out.println("Your maintenance costs for Spring($" + springCosts + ") Summer($" + summerCosts + ") Fall($" + fallCosts + ") and Winter($" + winterCosts + ") total to a yearly maintenance cost of $" + totalCosts);
+    }
+}
